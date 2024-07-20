7c8e89b19ea399be7e7e9313a5eae48c1cea718e
Sana Ahsan <sanaahsanmallick@gmail.com>
2024-07-20 09:08:29 +0530

    ADDED

---
+++ MembershipOperator.py
@@ -0,0 +1,19 @@
+# MEMBERSHIP OPERATOR
+
+# 1) in
+
+a=10;
+list=[1,2,3,4,5];
+if(a in list):
+    print("Yes");
+else:
+    print("No");
+
+# 2) not in
+
+a=50;
+list=[10,20,30,40,50];
+if(a not in list):
+    print("a is not in list");
+else:
+    print("A is present in list");
