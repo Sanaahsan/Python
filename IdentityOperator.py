1ce7d86eb55a7ec8754d38a94e24401d3a0c60c2
Sana Ahsan <sanaahsanmallick@gmail.com>
2024-07-20 09:07:15 +0530

    ADDED

---
+++ IdentityOperator.py
@@ -0,0 +1,17 @@
+# IDENTITY OPERATOR
+
+# 1) is
+
+a=5;
+if(type(a)is int):
+    print("correct");
+else:
+    print("Incorrect");
+
+# 2) is not
+
+a = 'sana';
+if(type(a)is not int):
+    print("Incorrect");
+else:
+    print("Correct");
