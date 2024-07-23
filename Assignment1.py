2be8f33bd9f4bcc5a261e1accfb2f070c80b51cc
Sana Ahsan <sanaahsanmallick@gmail.com>
2024-07-23 12:14:06 +0530

    UPDATED

--- Assignment1.py
+++ Assignment1.py
@@ -6,6 +6,7 @@ Discount= unit*0.10;
 Bill=unit-Discount;
 print(Bill);
 
+
 # WAP to accept marks of 5 subjects and find total marks and % assuming full marks as 100 in each subject.
 
 Maths=int(input("Enter Maths marks:"));
@@ -18,5 +19,43 @@ Total=Maths+English+Physics+Biology+Computer;
 Percentage= Total/500*100;
 print(Percentage);
 
+
+
 # WAP to swap values of two numbers
+
+
+# WAP to accept total sales amount and find profit amount @5%
+
+# WAP to check whether an 'age' is eligible for voting or not
+
+age=int(input("Enter your age"));
+if age>=18:
+    print("Eligible");
+else:
+    print("Not eligible");
+
+
+
+# WAP to find max between two numbers
+
+a=int(input("Enter 1st number:"));
+b=int(input("Enter 2nd number:"));
+if a>b:
+    print("a is greater than b");
+else:
+    print("b is greater than a");
+
+
+# WAP to check whether a given number is positive, negative or 0
+
+a=float(input("Enter your number"));
+if a>0:
+    print("Positive");
+elif a<0:
+    print("Negative");
+else:
+    print("Zero");
+
+# WAP to find the middle number
+
 
