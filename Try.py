6efd6cbad75cc031eddc0b45a3861f208c7fe9b9
Sana Ahsan <sanaahsanmallick@gmail.com>
2024-07-24 19:31:48 +0530

    UPDATED

--- Try.py
+++ Try.py
@@ -1,14 +1,17 @@
-# WAP to find products of digits of a given number
 
-num=int(input("Enter your number"))
-sum=0
-product=1
-while(num>0):
-    digit=num%10
-    if(digit%2==0):
-        sum=sum+digit
-    else:
-        product=product*digit
-    num=num//10;
-print(sum);
-print(product)        
+# Plaindrome 525
+
+n=int(input("Enter your number:"));
+original=n;
+rev=0;
+while(n>0):
+    rev=(rev*10)+n%10
+    n=n//10;
+if (original==rev):
+    print("Yes");
+else:
+    print("No")    
+
+
+
+
