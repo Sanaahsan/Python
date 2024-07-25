a0f2283adec747be46286a1dec06360e39fbd21b
Sana Ahsan <sanaahsanmallick@gmail.com>
2024-07-25 20:53:32 +0530

    UPDATED

--- While_loop.py
+++ While_loop.py
@@ -186,7 +186,23 @@ else:
 
 
 # WAP to check whether a given number is pime or not
+n=int(input("Enter your number:"))
+i=1;
+count=0;
+while(i<=n):
+    if(n%i==0):
+        count=count+1
+    i=i+1;
+if(count==2):
+    print("It is a prime number")
+else:
+    print("It is not a prime number")        
+
+
+
 # WAP to print factorial of a given number
+
+
 # WAP to print fibonacci series upto a given number
 
 
