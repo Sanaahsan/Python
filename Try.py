c367b66253e44e923e5b6452b3443146c660ef20
Sana Ahsan <sanaahsanmallick@gmail.com>
2024-07-27 21:06:33 +0530

    UPDATED

--- Try.py
+++ Try.py
@@ -1,13 +1,8 @@
-
-# Prime or not
-n=int(input("Enter your number:"))
-i=1;
-count=0;
-while(i<=n):
-    if(n%i==0):
-        count=count+1
-    i=i+1;
-if(count==2):
-    print("It is a prime number")
-else:
-    print("It is not a prime number")        
+i=1
+while(i<5):
+    j=1
+    while(j<5):
+        print(j, end="")
+        j=j+1
+    print()    
+    i=i+1
